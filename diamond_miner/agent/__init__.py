e6ef5025f6c98a69015c206c73ef26f74eecbd9d
matthieugouel <matthieu.gouel@gmail.com>
2020-06-25 10:46:29 +0200

    feat(monitoring): basic log/metrics monitoring stack

--- diamond_miner/agent/__init__.py
+++ diamond_miner/agent/__init__.py
@@ -1,4 +1,11 @@
 import logging
+import logging_loki
+
+from diamond_miner.agent.settings import AgentSettings
+from multiprocessing import Queue
+
+settings = AgentSettings()
+
 
 logger = logging.getLogger("agent")
 logger.setLevel(logging.INFO)
@@ -7,4 +14,10 @@ stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.INFO)
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
+
+loki_handler = logging_loki.LokiQueueHandler(
+    Queue(-1), url=settings.LOKI_URL, version=settings.LOKI_VERSION
+)
+logger.addHandler(loki_handler)
+
 logger.propagate = False
