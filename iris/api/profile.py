11b59504f2f2032176faf099a70e20a1218ebbf9
matthieugouel <matthieu.gouel@protonmail.com>
2021-03-22 10:10:43 +0100

    feat(api): oauth2 authentication

--- iris/api/profile.py
+++ iris/api/profile.py
@@ -1,16 +1,52 @@
 """Profile operations."""
 
+from datetime import timedelta
+
 from fastapi import APIRouter, Depends, HTTPException, Request, status
+from fastapi.security import OAuth2PasswordRequestForm
+from pydantic import BaseModel
 
 from iris.api.schemas import (
     ProfileGetResponse,
     ProfileRIPEPutBody,
     ProfileRIPEPutResponse,
 )
-from iris.api.security import authenticate
+from iris.api.security import (
+    authenticate_user,
+    create_access_token,
+    get_current_active_user,
+)
 from iris.commons.database import DatabaseUsers, get_session
 
 router = APIRouter()
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+@router.post("/token", response_model=Token)
+async def get_token(
+    request: Request,
+    form_data: OAuth2PasswordRequestForm = Depends(),
+):
+    user = await authenticate_user(request, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(
+        minutes=request.app.settings.API_TOKEN_EXPIRATION_TIME
+    )
+    access_token = create_access_token(
+        request, data={"sub": user["username"]}, expires_delta=access_token_expires
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 @router.get(
@@ -20,18 +56,14 @@ router = APIRouter()
 )
 async def get_profile(
     request: Request,
-    username: str = Depends(authenticate),
+    user: str = Depends(get_current_active_user),
 ):
     """Get profile information."""
-    session = get_session(request.app.settings)
-    users_database = DatabaseUsers(session, request.app.settings, request.app.logger)
-
-    profile_info = await users_database.get(username)
-    profile_info["ripe"] = {
-        "account": profile_info["ripe_account"],
-        "key": profile_info["ripe_key"],
+    user["ripe"] = {
+        "account": user["ripe_account"],
+        "key": user["ripe_key"],
     }
-    return profile_info
+    return user
 
 
 @router.put(
@@ -43,7 +75,7 @@ async def get_profile(
 async def put_ripe_profile(
     request: Request,
     ripe_info: ProfileRIPEPutBody,
-    username: str = Depends(authenticate),
+    user: str = Depends(get_current_active_user),
 ):
     session = get_session(request.app.settings)
     users_database = DatabaseUsers(session, request.app.settings, request.app.logger)
@@ -54,5 +86,7 @@ async def put_ripe_profile(
             detail="`account` and `key` must have the same type",
         )
 
-    await users_database.register_ripe(username, ripe_info.account, ripe_info.key)
+    await users_database.register_ripe(
+        user["username"], ripe_info.account, ripe_info.key
+    )
     return {"account": ripe_info.account, "key": ripe_info.key}
--- iris/api/security.py
+++ iris/api/security.py
@@ -1,31 +1,85 @@
 """Authentication and security handlers."""
 
+from datetime import datetime, timedelta
+from typing import Optional
 
 from fastapi import Depends, HTTPException, Request, status
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
 from passlib.context import CryptContext
 
 from iris.commons.database import DatabaseUsers, get_session
 
-security = HTTPBasic()
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="v0/profile/token")
 
 
-async def authenticate(
-    request: Request, credentials: HTTPBasicCredentials = Depends(security)
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def create_access_token(
+    request: Request, data: dict, expires_delta: Optional[timedelta] = None
 ):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode,
+        request.app.settings.API_TOKEN_SECRET_KEY,
+        algorithm=request.app.settings.API_TOKEN_ALGORITHM,
+    )
+    return encoded_jwt
+
+
+async def get_user(request: Request, username: str):
     session = get_session(request.app.settings)
     database = DatabaseUsers(session, request.app.settings, request.app.logger)
-    user = await database.get(credentials.username)
+    return await database.get(username)
 
-    is_verified = False
-    if user is not None:
-        is_verified = pwd_context.verify(credentials.password, user["hashed_password"])
 
-    if not is_verified:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Basic"},
+async def authenticate_user(request: Request, username: str, password: str):
+    user = await get_user(request, username)
+
+    if not user:
+        return False
+    if not verify_password(password, user["hashed_password"]):
+        return False
+
+    return user
+
+
+async def get_current_user(request: Request, token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(
+            token,
+            request.app.settings.API_TOKEN_SECRET_KEY,
+            algorithms=[request.app.settings.API_TOKEN_ALGORITHM],
         )
-    return credentials.username
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = await get_user(request, username=username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user=Depends(get_current_user)):
+    if not current_user["is_active"]:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
