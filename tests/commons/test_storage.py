1a00ecbc2b3d93e89fe94f6f4d8b1c55add52c55
Maxime Mouchet <max@maxmouchet.com>
2021-07-23 00:12:29 +0200

    test(storage): test every storage functions

--- tests/commons/test_storage.py
+++ tests/commons/test_storage.py
@@ -1,32 +1,76 @@
-from io import BytesIO
-
 import pytest
 
 from iris.commons.storage import Storage
 
 
 @pytest.mark.asyncio
-async def test_storage(common_settings):
+async def test_buckets(common_settings):
     storage = Storage(settings=common_settings)
 
     assert await storage.create_bucket("bucket1") is None
+    # Creating the same bucket twice should not fail
     assert await storage.create_bucket("bucket2") is None
+    assert await storage.create_bucket("bucket2") is None
+    # "Infrastructure" buckets must be ignored
+    assert await storage.create_bucket("targets") is None
     assert await storage.get_measurement_buckets() == ["bucket1", "bucket2"]
-
-    file = BytesIO(b"1234")
-    await storage.upload_file_no_retry(
-        "bucket1", "1234.txt", file, metadata={"meta": "data"}
-    )
-
-    file = await storage.get_file_no_retry("bucket1", "1234.txt")
-    assert file["key"] == "1234.txt"
-    assert file["content"] == "1234"
-    assert file["metadata"] == {"meta": "data"}
-
-    await storage.delete_file_check_no_retry("bucket1", "1234.txt")
-    with pytest.raises(Exception):
-        await storage.delete_file_check_no_retry("bucket1", "1234.txt")
 
     assert await storage.delete_bucket("bucket1") is None
     assert await storage.delete_bucket("bucket2") is None
     assert await storage.get_measurement_buckets() == []
+
+
+@pytest.mark.asyncio
+async def test_files(common_settings, tmp_path):
+    storage = Storage(settings=common_settings)
+    await storage.create_bucket("bucket1")
+    await storage.create_bucket("bucket2")
+
+    content = "1234"
+    metadata = {"meta": "data"}
+    tmp_file = tmp_path / "file.txt"
+    tmp_file.write_text(content)
+
+    # Upload
+    await storage.upload_file_no_retry(
+        "bucket1", "1.txt", tmp_file.open("rb"), metadata
+    )
+    await storage.upload_file("bucket1", "2.txt", tmp_file, metadata)
+
+    # List
+    assert len(await storage.get_all_files_no_retry("bucket1")) == 2
+
+    # Get
+    for key in ["1.txt", "2.txt"]:
+        file = await storage.get_file_no_retry("bucket1", key)
+        assert file["key"] == key
+        assert file["content"] == content
+        assert file["metadata"] == metadata
+
+    # Download
+    download_file = tmp_path / "download.txt"
+    await storage.download_file("bucket1", "1.txt", download_file)
+    assert download_file.read_text() == content
+
+    # Copy
+    await storage.copy_file_to_bucket("bucket1", "bucket2", "1.txt", "1.txt")
+    file1 = await storage.get_file_no_retry("bucket1", "1.txt")
+    file2 = await storage.get_file_no_retry("bucket2", "1.txt")
+    # TODO: metadata are not copied currently, is this a bug?
+    # ["content", "key", "metadat]
+    for key in ["content", "key"]:
+        assert file1[key] == file2[key]
+
+    # Deletion
+    await storage.delete_file_check_no_retry("bucket1", "1.txt")
+    with pytest.raises(Exception):
+        await storage.delete_file_check_no_retry("bucket1", "1.txt")
+
+    await storage.delete_file_no_check("bucket1", "2.txt")
+    await storage.delete_file_no_check("bucket1", "2.txt")
+
+    assert len(await storage.get_all_files_no_retry("bucket1")) == 0
+    assert len(await storage.get_all_files_no_retry("bucket2")) == 1
+
+    await storage.delete_all_files_from_bucket("bucket2")
+    assert len(await storage.get_all_files_no_retry("bucket2")) == 0
