48b2b43e82527a4e09618fd13cbb1000671b3f62
Maxime Mouchet <max@maxmouchet.com>
2021-11-17 13:25:28 +0100

    feat(worker): add REDIS_NAMESPACE setting

--- iris/worker/__init__.py
+++ iris/worker/__init__.py
@@ -4,6 +4,6 @@ from dramatiq.brokers.redis import RedisBroker
 from iris.worker.settings import WorkerSettings
 
 settings = WorkerSettings()
-redis_broker = RedisBroker(url=settings.REDIS_URL)
+redis_broker = RedisBroker(namespace=settings.REDIS_NAMESPACE, url=settings.REDIS_URL)
 dramatiq.set_broker(redis_broker)
 dramatiq.set_encoder(dramatiq.PickleEncoder())
