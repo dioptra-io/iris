7ed43745be3c4b8f32595363e31ac78368b4af8c
matthieugouel <matthieu.gouel@protonmail.com>
2021-12-10 13:32:35 +0100

    fix(api): remove verify and enable routes

--- iris/api/users.py
+++ iris/api/users.py
@@ -1,9 +1,7 @@
 """Users operations."""
 
-from uuid import UUID
-
 import databases
-from fastapi import APIRouter, Depends, HTTPException, Query, Request, status
+from fastapi import APIRouter, Depends, Query, Request
 from httpx_oauth.clients.github import GitHubOAuth2
 
 from iris.api.authentication import current_superuser, fastapi_users, jwt_authentication
@@ -59,56 +57,3 @@ async def get_users(
     users = [public.User(**dict(user)) for user in users]
     querier = ListPagination(users, request, offset, limit)
     return await querier.query()
-
-
-@router.patch(
-    "/users/verify/{id}",
-    response_model=public.User,
-    summary="Verify a user (Admin only).",
-    tags=["Users"],
-)
-async def verify_user(
-    request: Request,
-    id: UUID,
-    user: public.UserDB = Depends(current_superuser),
-    sqlalchemy: databases.Database = Depends(get_sqlalchemy),
-):
-    """Verify a user specified by UUID."""
-    await sqlalchemy.execute(
-        "UPDATE user SET is_verified = true WHERE id = :id", values={"id": str(id)}
-    )
-    user = await sqlalchemy.fetch_one(
-        "SELECT * FROM user WHERE id = :id", values={"id": str(id)}
-    )
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
-        )
-    return dict(user)
-
-
-@router.patch(
-    "/users/enable/{id}",
-    response_model=public.User,
-    summary="Enable probing capabilities for a user (Admin only).",
-    tags=["Users"],
-)
-async def enable_probing_user(
-    request: Request,
-    id: UUID,
-    user: public.UserDB = Depends(current_superuser),
-    sqlalchemy: databases.Database = Depends(get_sqlalchemy),
-):
-    """Enable probing capabilities for a user specified by UUID."""
-    await sqlalchemy.execute(
-        "UPDATE user SET probing_enabled = true, probing_limit = :limit WHERE id = :id",
-        values={"limit": str(settings.API_DEFAULT_PROBING_LIMIT), "id": str(id)},
-    )
-    user = await sqlalchemy.fetch_one(
-        "SELECT * FROM user WHERE id = :id", values={"id": str(id)}
-    )
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
-        )
-    return dict(user)
