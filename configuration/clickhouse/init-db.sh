180451f1813b63bd1adcd2168f6354648c0fb588
Maxime Mouchet <max@maxmouchet.com>
2022-08-25 13:24:10 +0200

    feat(docker): grant permissions required by iris-exporter in init-db.sh

--- configuration/clickhouse/init-db.sh
+++ configuration/clickhouse/init-db.sh
@@ -8,4 +8,7 @@ clickhouse client -n <<-EOSQL
     CREATE DATABASE IF NOT EXISTS iris_test;
     GRANT ALL ON iris.* TO iris WITH GRANT OPTION;
     GRANT ALL ON iris_test.* TO iris WITH GRANT OPTION;
+    -- These permissions are necessary for testing iris-exporter
+    -- without creating a dedicated iris-exporter user.
+    GRANT CREATE TEMPORARY TABLE, S3 ON *.* TO iris;
 EOSQL
