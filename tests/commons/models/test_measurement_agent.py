dd6a77cec160f221a32aa270aaea300ea2eef8e2
Elenetta17 <elena.nardi@lip6.fr>
2025-11-05 14:48:20 +0100

    update tests

--- tests/commons/models/test_measurement_agent.py
+++ tests/commons/models/test_measurement_agent.py
@@ -4,22 +4,20 @@ from pydantic import ValidationError
 from iris.commons.models import MeasurementAgentCreate
 
 def test_create_missing_tag_uuid():
+    """Fail when both uuid and tag are missing."""
     with pytest.raises(ValidationError) as exc_info:
         MeasurementAgentCreate()
 
     errors = exc_info.value.errors()
-    assert any(
-        error["loc"] == ("__root__",) and "one of `uuid` or `tag`" in error["msg"]
-        for error in errors
-    )
+    # Check that at least one error message mentions 'one of `uuid` or `tag`'
+    assert any("one of `uuid` or `tag`" in error["msg"] for error in errors)
 
 
 def test_create_tag_and_uuid():
+    """Fail when both uuid and tag are provided simultaneously."""
     with pytest.raises(ValidationError) as exc_info:
         MeasurementAgentCreate(tag="tag", uuid="uuid")
 
     errors = exc_info.value.errors()
-    assert any(
-        error["loc"] == ("__root__",) and "one of `uuid` or `tag`" in error["msg"]
-        for error in errors
-    )
+    assert any("one of `uuid` or `tag`" in error["msg"] for error in errors)
+
